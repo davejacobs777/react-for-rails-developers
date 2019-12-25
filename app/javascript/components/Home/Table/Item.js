80748df5d4d0d2b5790593c7060b944d25a4eeef
David Jacobsen <dgrjacobsen@gmail.com>
2019-12-25 04:25:32 +0100

    onClick handler created to change state for ActiveItem

--- app/javascript/components/Home/Table/Item.js
+++ app/javascript/components/Home/Table/Item.js
@@ -32,7 +32,7 @@ const Item = (props) => {
                                     <h4>{props.title}</h4>
                                     <p>{props.description}</p>
                                     <div className="cta-wrapper">
-                                        <Button href="#" className="btn cta-btn">Watch This Video</Button>
+                                        <Button onClick={props.handleVideoChange} href="#" className="btn cta-btn">Watch This Video</Button>
                                     </div>
                                 </div>
                             </div>
